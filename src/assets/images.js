990a2e6b2822ddb587d2e5abca58c34dd07949ea
Anna Petelin <andorjamb@protonmail.com>
2023-03-27 11:24:32 +0300

    edit carousel

--- src/assets/images.js
+++ src/assets/images.js
@@ -1,21 +1,21 @@
-import japan from './japan.jpg';
-import namibia from './namibia.jpg';
-import norway from './norway.jpeg';
+import england from './images/england.jpg';
+import australia from './images/australia.jpg';
+import nepal from './images/nepal.jpg';
+import cameroon from './images/cameroon.jpg';
+import india from './images/india.jpg';
+import usa from './images/usa.jpg';
+import bangladesh from './images/bangladesh.jpg';
+import vietnam from './images/vietnam.jpg';
+import azerbaijan from './images/azerbaijan.jpg';
+import estonia from './images/estonia.jpg';
+import russia from './images/russia.jpg';
+import kenya from './images/kenya.jpg';
+import nigeria from './images/nigeria.jpg';
+import finland from './images/finland.jpg';
 
-import england from './england.jpg';
-import australia from './australia.jpg';
-import nepal from './nepal.jpg';
-import cameroon from './cameroon.jpg';
-import india from './india.jpg';
-import usa from './usa.jpg';
-import bangladesh from './bangladesh.jpg';
-import vietnam from './vietnam.jpg';
-import azerbaijan from './azerbaijan.jpg';
-import estonia from './estonia.jpg';
-import russia from './russia.jpg';
-import kenya from './kenya.jpg';
-import nigeria from './nigeria.jpg';
-import finland from './finland.jpg';
+const images = [{
+    file: england,
+    alt: "England"
+}, { file: australia, alt: "Australia" }, { file: nepal, alt: "Nepal" }, { file: cameroon, alt: "Cameroon" }, { file: india, alt: "India" }, { file: usa, alt: "USA" }, { file: bangladesh, alt: "Bangladesh" }, { file: vietnam, alt: "Vietnam" }, { file: azerbaijan, alt: "Azerbaijan" }, { file: estonia, alt: "Estonia" }, { file: russia, alt: "Russia" }, { file: kenya, alt: "Kenya" }, { file: nigeria, alt: "Nigeria" }, { file: finland, alt: "Finland" }]
 
-const images = [england, australia, nepal, cameroon, india, usa, bangladesh, vietnam, azerbaijan, estonia, russia, kenya, nigeria, finland]
 export default images 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,16 +4,17 @@ import images from '../assets/images';
 import '../customStyles/Home.css';
 
 const Home = () => {
+
   return (
     <div className="home-container">
       <Row>
         <Col>
           <div className="info-container"><Card id="info">
-            <Card.Body>
+            <Card.Body><Card.Title className="justify-content-center">About</Card.Title>
               <Card.Text>'Countries' is a simple React application made as an end-of-term assignment at
-                Business College Helsinki. The app makes use of {' '}
-                <a href="https://restcountries.com/">https://restcountries.com/ </a> and{' '}
-                <a href="https://openweathermap.org/">https://openweathermap.org/</a> APIs.</Card.Text>
+                Business College Helsinki. The app makes use of
+                <a href="https://restcountries.com/"> REST countries </a> and
+                <a href="https://openweathermap.org/"> Open Weather Map </a> APIs.</Card.Text>
             </Card.Body>
           </Card>
           </div>
@@ -22,17 +23,10 @@ const Home = () => {
           <Carousel className='home-carousel' slide="true">
             {images.map((image) => (
               <Carousel.Item className='.carousel-item'>
-                <img src={image} className="d-block w-100 home-carousel" alt="country" />
+                <img src={image.file} className="d-block w-100 home-carousel" alt={image.alt} title={image.alt} />
               </Carousel.Item >
             ))}
-            {/*       <Carousel.Item className='.carousel-item'>
-              <img src={images[0]} className="d-block w-100 home-carousel" alt="Norwegian mountain" />
-            </Carousel.Item >
-            <img src={images[1]} className="d-block w-100 home-carousel" alt="Namibian desert" />
-            <Carousel.Item className='.carousel-box'>
-              <img src={images[2]} className="d-block w-100" alt="Boat in Japan" />
-            </Carousel.Item>
-            <Carousel.Item className='.carousel-box'></Carousel.Item> */}
+            <Carousel.Item className='.carousel-box'></Carousel.Item>
           </Carousel>
         </Col>
       </Row>
